a4d7e92c5e172761374a26505af76d9df7a58252
Nazar Buzyl <nazarbuzyl@gmail.com>
2021-07-26 00:26:43 +0300

    login-from

--- students/buzyl_nazar/final-project/src/js/script.js
+++ students/buzyl_nazar/final-project/src/js/script.js
@@ -1,7 +1,15 @@
-
+import $ from 'jquery';
 
 // Burger btn
 let btn = document.querySelector('#burgerMenuBtnItem');
-        btn.addEventListener('click', function () {
-            document.documentElement.classList.toggle('menu-open');
-        });
+btn.addEventListener('click', function () {
+    document.documentElement.classList.toggle('menu-open');
+});
+
+// Open - Close
+loginOpen.addEventListener("click", function () {
+    document.getElementById("loginForm").style.display = "block";
+});
+loginClose.addEventListener("click", function () {
+    document.getElementById("loginForm").style.display = "none";
+});
