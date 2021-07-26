5f10ce13186a3b12052acc64e7af0f31c8fc0387
Nazar Buzyl <nazarbuzyl@gmail.com>
2021-07-26 18:58:53 +0300

    change color

---
+++ students/buzyl_nazar/final-project/build/js/product-list.js
@@ -0,0 +1,35 @@
+
+const catalogTitle = [
+    {
+        id: 1,
+        title: 'Висувні роторні дощувачі',
+        img: '',
+    },
+    {
+        id: 2,
+        title: '...',
+        img: '',
+    },
+];
+const productCatalog = [
+    {
+        id: 1,
+        title: 'PGJ- 04',
+        img: '',
+        price: '',
+        specialPrice: '',
+        height: 10,
+        regulationSector: 'Регульований сектор 40-360º',
+        characteristics: 'Регульований сектор 40-360º, Н = 10 см, без зворотн. клап.',
+    },
+    {
+        id: 2,
+        title: '',
+        img: '',
+        price: '',
+        specialPrice: '',
+        height: 10,
+        regulationSector: '',
+        characteristics: '',
+    },
+];
--- students/buzyl_nazar/final-project/src/js/script.js
+++ students/buzyl_nazar/final-project/src/js/script.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import './login-form';
 
 // Burger btn
 let btn = document.querySelector('#burgerMenuBtnItem');
@@ -6,10 +7,42 @@ btn.addEventListener('click', function () {
     document.documentElement.classList.toggle('menu-open');
 });
 
-// Open - Close
-loginOpen.addEventListener("click", function () {
-    document.getElementById("loginForm").style.display = "block";
-});
-loginClose.addEventListener("click", function () {
-    document.getElementById("loginForm").style.display = "none";
-});
+// Open - Close Login - Form
+
+// loginOpen.addEventListener("click", function () {
+//     document.getElementById("loginForm").style.display = "block";
+//     document.documentElement.classList.add('login-open');
+
+// });
+// loginClose.addEventListener("click", function () {
+//     document.getElementById("loginForm").style.display = "none";
+//     document.documentElement.classList.remove('login-open');
+// });
+
+const loginOpen = document.querySelector('#loginOpen');
+const formPopup = document.querySelector('#loginForm');
+const loginClose = document.querySelector('#loginClose');
+loginOpen.onclick = function (e) {
+    formPopup.style.display = 'block';
+    let elems = document.querySelectorAll('.alert-validate');
+    [].forEach.call(elems, function (el) {
+        el.classList.remove("alert-validate");
+    });
+}
+loginClose.onclick = function (e) {
+    formPopup.style.display = 'none';
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+}
+// $(document).click(function (e) {
+//     if (!loginBtn.is(e.target) && !menu.is(e.target) && menu.has(e.target).length === 0) {
+//         menu.slideUp();
+//         menuBtn.removeClass('is-active');
+//     };
+// });
+
+// Cart
+// cartOpen.addEventListener("click", function () {
+//     document.getElementById("cartProducts").style.visibilit = "visible";
+//     document.getElementById("cartProducts").style.opacity = "1";
+// });
